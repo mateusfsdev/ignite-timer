13614691a585b147857e271153f698e169ac1d15
Mateus F dev <mateusfdev@outlook.com>
2024-02-20 18:08:16 -0300

    context class 💥💢💥💢💥

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,28 +1,41 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useState } from "react"
 
-const CycleContext = createContext({
-  activeCycle: 6,
-})
+const CycleContext = createContext({} as any)
 
 function NewCycleForm(){
-  const { activeCycle } = useContext(CycleContext)
+  const { activeCycle, setActiveCycle } = useContext(CycleContext)
 
-  return <h1>new cycle: {activeCycle}</h1>
+  return <h1>new cycle: {activeCycle}
+  <button onClick={() => {
+      setActiveCycle(activeCycle + 1)
+    }}
+     >alterar ciclo atico
+     </button>
+    </h1>
 }
 
 function Countdown(){
   const { activeCycle } = useContext(CycleContext)
 
-  return <h1>countdown: {activeCycle}</h1>
-}
+  return(
+    <h1>countdown: {activeCycle}
+     </h1>
+  
+
+  )}
 
 
 
 export function Home(){
+
+  const [activeCycle, setActiveCycle] = useState(0)
+
   return (
+   <CycleContext.Provider value={{activeCycle, setActiveCycle}}>
     <div>
       <NewCycleForm />
       <Countdown />
     </div>
+    </CycleContext.Provider>
   )
 }
